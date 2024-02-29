2cf3c1e0655a7cf978fdf8b0b01057eee4e5df75
232Ahmed232 <x99aliahmed@gmail.com>
2024-02-29 09:20:55 +0500

    utility made

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 import dotenv from "dotenv"
 
 dotenv.config({
@@ -8,3 +8,11 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server is running");
+    })
+})
+.catch((err)=>{
+    console.log("Mongo failed to connect: " ,err);
+})
